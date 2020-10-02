6623847ebfa1823586ba247975de99438fbe0402
Arkadiusz Masiakiewicz <arkadiusz.masiakiewicz@avra.pl>
2020-10-02 17:16:16 +0200

    adding swagger doc

--- src/main/java/com/casinoroyale/player/PlayerApplication.java
+++ src/main/java/com/casinoroyale/player/PlayerApplication.java
@@ -8,6 +8,10 @@ import javax.validation.ClockProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class PlayerApplication {
@@ -21,6 +25,15 @@ public class PlayerApplication {
     @Bean
     ClockProvider clockProvider() {
         return () -> Clock.system(DEFAULT_ZONE_OFFSET);
+    }
+
+    @Bean
+    Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
